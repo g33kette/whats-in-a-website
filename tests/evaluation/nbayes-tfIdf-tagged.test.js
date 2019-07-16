82fe06750eb513bec0bdfacaaeafca262359d70d
Joanne Buxton <j.buxton@uea.ac.uk>
2019-07-16 17:31:12 +0100

    New tests and results

---
+++ tests/evaluation/nbayes-tfIdf-tagged.test.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+
+import {resetStore, trainFromFiles, runClassificationEvaluation} from '../utils';
+import {config as modelConfig} from '../../src/services/model';
+
+jest.setTimeout(14400000); // 4 hours
+
+const vectorType = 'tfIdf';
+const wordType = 'tagged';
+const limit = 0;
+modelConfig.modelType = 'nbayes';
+
+beforeAll(async () => {
+    console.log('Start Run: '+new Date());
+    await resetStore();
+    await trainFromFiles(
+        '/tests/data/training/safe',
+        '/tests/data/training/harmful',
+        vectorType,
+        wordType,
+        limit
+    );
+});
+
+beforeEach(async () => {
+    console.log('Start Scenario: '+new Date());
+});
+
+afterAll( async () => {
+    await resetStore();
+    console.log('Complete Run: '+new Date());
+});
+
+it('Should run evaluation of model against test data', async () => {
+    await runClassificationEvaluation('/tests/data/testing', vectorType, wordType, 1);
+});
+
+// Not to be run until final evaluation
+// it('Should run evaluation of model against validation data', async () => {
+//     await runClassificationEvaluation('/tests/data/validation', vectorType, wordType, limit);
+// });
